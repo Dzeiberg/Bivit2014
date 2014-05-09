65e141887efe74e442803e3010a8b52af70ed4b7
Dzeiberg <dzeiberg@me.com>
2014-05-09 13:53:41 -0400

    Add routes

---
+++ db/migrate/20140508173504_create_microposts.rb
@@ -0,0 +1,13 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    create_table :microposts do |t|
+      t.string :content
+      t.integer :user_id
+      t.string :title
+      t.string :takeaways
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+end
