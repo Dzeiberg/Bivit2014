86d99d45d128f883469cb951bd86e1c62d94998e
Abhay <abhay@ubuntu.ubuntu-domain>
2014-05-15 12:27:16 -0400

    abhay 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,11 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
   end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-   def create
+  def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       sign_in @user
@@ -23,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-      params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
+  end
 end
