4ef9b9f472b838a74f9fcaa02cdfa79ed8e48f0e
Dzeiberg <dzeiberg@me.com>
2014-05-15 22:06:06 -0400

    Revert 0db13af..add386d
    This rolls back to commit 0db13af8fe42f527d47d11bed1ceedd6576c157b.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,8 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
   end
-  def edit
-    @user = User.find(params[:id])
-  end
 
-  def create
+   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       sign_in @user
@@ -26,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,13 +4,13 @@ describe "User pages" do
 
   subject { page }
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+describe "profile page" do
+  let(:user) { FactoryGirl.create(:user) }
+  before { visit user_path(user) }
 
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
+  it { should have_content(user.name) }
+  it { should have_title(user.name) }
+end
 
   describe "signup page" do
     before { visit signup_path }
@@ -18,7 +18,6 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title('Sign up') }
   end
-
   describe "signup" do
      before { visit signup_path }
 
@@ -50,22 +49,6 @@ describe "User pages" do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
-    end
-  end
-  describe "edit" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
-
-    describe "page" do
-      it { should have_content("Update your profile") }
-      it { should have_title("Edit user") }
-      it { should have_link('change', href: 'http://gravatar.com/emails') }
-    end
-
-    describe "with invalid information" do
-      before { click_button "Save changes" }
-
-      it { should have_content('error') }
     end
   end
 end
